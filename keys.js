e85859c25e3a095d0d7bd251d8bf004c197de92e
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-02-14 20:30:15 -0800

    twitterworks

---
+++ keys.js
@@ -0,0 +1,11 @@
+// JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '62ja8w19aYxJrBQM6BhhUtTxv',
+  consumer_secret: 'hp9jdje4C9laeK8lEcbCeoVKrqU4DuD2oia73KAbLkngiG7Lx4',
+  access_token_key: '858181639985184768-I7UzEXinxmjhA2W2NipDphlazRq84Vx',
+  access_token_secret: 'zs2hXIIL5w2Dczb79KTUmZfJbXUhH6M43uZnQiA3WVfF7',
+}
+
+module.exports = twitterKeys;
