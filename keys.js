40604ef50bde1205358b9c33aed4e169171b7bc8
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-02-14 23:17:00 -0800

    keys updated

--- keys.js
+++ keys.js
@@ -7,5 +7,13 @@ var twitterKeys = {
   access_token_key: '858181639985184768-I7UzEXinxmjhA2W2NipDphlazRq84Vx',
   access_token_secret: 'zs2hXIIL5w2Dczb79KTUmZfJbXUhH6M43uZnQiA3WVfF7',
 }
-
+var spotifyKeys = {
+  	id: '294138ad16734a9eb79f0d7bf6b2e67d',
+  	secret: 'd821b6c01074460797a3085215b58927'
+};
+var OMDBAPIKey = {
+	'apikey=40e9cece'
+}
 module.exports = twitterKeys;
+module.exports = spotifyKeys;
+module.exports = OMDBAPIKey;
